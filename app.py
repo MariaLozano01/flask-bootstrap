d14d35afc0642f094e35efb89bd17aa7594befb2
MariaLozano01 <102619474+MariaLozano01@users.noreply.github.com>
2022-09-18 23:33:03 -0400

    updates

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+from flask import Flask, render_template
+app = Flask(__name__)
 
+@app.route('/')
+def homepage():
+    return render_template('Style1.html')
+
+@app.route('/Page2')
+def homepage1():
+    return render_template('/Style2.html')
+
+if __name__== '__main__': 
+    app.run(debug=False, host='0.0.0.0', port=80)
